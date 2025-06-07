00da9974fde42fa5877f931d1ac2a06b792b9e2b
Pushkar Raj <px86@protonmail.com>
2025-06-07 21:06:45 +0530

    Add O(n) solution for `Two Integer Sum II` problem
    Use two pointer technique to find two elements that add up to a
    target in a sorted array. This is an improvement over my previous
    solution which was O(nlogn).

--- two-pointers/TwoIntegerSumII.py
+++ two-pointers/TwoIntegerSumII.py
@@ -1,14 +1,19 @@
 # https://neetcode.io/problems/two-integer-sum-ii?list=neetcode150
 
-import bisect
+from typing import List
 
 
 class Solution:
 
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i, num in enumerate(numbers):
-            difference = target - num
-            j = bisect.bisect_left(numbers, difference, lo=i + 1)
-            if j != len(numbers) and numbers[j] == difference:
-                return [i + 1, j + 1]
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left + 1, right + 1]  # 1 based index
+            elif sum > target:
+                right -= 1
+            else:
+                left += 1
         return [-1, -1]

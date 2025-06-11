b6402d09d3a68d85fc938906914a819634d5aa7c
Pushkar Raj <px86@protonmail.com>
2025-06-11 17:06:56 +0530

    Refactor solution of `Longest Substring Without Repeating Characters`

--- sliding-window/LongestSubstringWithoutRepeatingCharacters.py
+++ sliding-window/LongestSubstringWithoutRepeatingCharacters.py
@@ -4,16 +4,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         chars = set()
-        max_len = 0
+        maxlen = 0
         l = r = 0
-        while l <= r and r < len(s):
-            if s[r] not in chars:
-                chars.add(s[r])
-                win_len = r - l + 1
-                if win_len > max_len:
-                    max_len = win_len
-                r += 1
-            else:
+        for r in range(len(s)):
+            while s[r] in chars:
                 chars.remove(s[l])
                 l += 1
-        return max_len
+            chars.add(s[r])
+            winlen = r - l + 1
+            if winlen > maxlen:
+                maxlen = winlen
+            r += 1
+        return maxlen

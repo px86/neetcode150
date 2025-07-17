c078ea2d021ed7e1f8fb257937ed90270290dc75
Pushkar Raj <px86@protonmail.com>
2025-07-17 20:28:09 +0530

    Add solution for `Same Binary Tree` problem

---
+++ trees/SameBinaryTree.py
@@ -0,0 +1,23 @@
+# https://neetcode.io/problems/same-binary-tree?list=neetcode150
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == q and p is None:
+            return True
+        if not (p and q):
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
